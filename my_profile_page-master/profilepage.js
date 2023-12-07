f5d04d5fee2998506d490afe6d827ccc13200532
rupeshkumar06 <152691818+rupeshkumar06@users.noreply.github.com>
2023-12-07 11:54:29 +0530

    Add files via upload

---
+++ my_profile_page-master/profilepage.js
@@ -0,0 +1,9 @@
+document.getElementById("close-btn").onclick= function() {
+    document.getElementById("card").style.display= "none"
+    document.getElementById("appear").style.display= "block"
+}
+
+function appear() {
+    document.getElementById("card").style.display= "block"
+    document.getElementById("appear").style.display= "none"
+}
